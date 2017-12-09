14047bb695369a3040b5c2c1d8332cf17e77eb43
Kaleb Olson <kaleb.olson@gmail.com>
2017-12-09 13:46:47 -0700

    Added more metrics and removed features

--- model/experimental.py
+++ model/experimental.py
@@ -9,6 +9,7 @@ from keras.layers import Dense, Dropout
 from keras.utils import to_categorical
 
 from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import roc_auc_score, classification_report
 
 ordered_labels_full = [
                     'Benign', 
@@ -40,20 +41,27 @@ tweak_to_hot = lambda df,col: to_categorical(df[col].astype("category").cat.code
 def load_and_process(filename):
     data = pd.read_csv(filename)
 
-    inputdata = data[['Conservation', 'PALJ810102', 'CHOP780216',
-                    'ONEK900102', 'BIOV880101', 'FUKS010108', 'CEDJ970102', 'CHAM820101',
-                    'FAUJ880107', 'MAXF760104', 'ZIMJ680104', 'GEIM800109', 'GEOR030106',
-                    'KARS160111', 'DIGM050101', 'WERD780103', 'QIAN880115']]
+    # EXCLUDE THE MUTATIONS THAT RESULT IN EARLY TERMINATION
+    data = data[data["Mutated AA"] != "Ter"]
+
+    inputdata = data[['Conservation', 'CHOP780216',
+                    'ONEK900102', 'BIOV880101', 'FUKS010108', 
+                      'WERD780103' ]]
 
 
     # Convert categorical column to one hot
     unmut  = (tweak_to_hot(data, "Unmutated AA"))
     mut = (tweak_to_hot(data, "Mutated AA"))
 
+    # INCLUDE OR NOT THE MUTATED AA
     train = np.c_[unmut, inputdata.values, mut]
 
     # Reduce labels (from 7 to 3)
     reduced_labels = data["Clinical Significance"].replace(reduced_labels_mapping)
+
+    # Write out comparable csv
+    csvout = inputdata.join(data["Unmutated AA"]).join(data["Mutated AA"]).join(reduced_labels)
+    csvout.to_csv("pout.csv")
 
     # Convert label to ordered category
     label_cat = pd.Categorical(reduced_labels, categories=ordered_labels_reduced, ordered=True)
@@ -94,7 +102,14 @@ def train_and_evaluate_model(model, train, train_label, test, test_label):
 
     loss_and_metrics_train = np.array(model.evaluate(train, train_label, batch_size=128))
     loss_and_metrics_test = np.array(model.evaluate(test, test_label, batch_size=128))
-    return loss_and_metrics_train, loss_and_metrics_test
+
+    predictions = model.predict(test)
+    auc_score = roc_auc_score(test_label, predictions)
+    report = classification_report(np.argmax(test_label, axis=1), np.argmax(predictions, axis=1), target_names=ordered_labels_reduced)
+    # Ugly hack to get precsion, recall, f1, support
+    pr_scores = np.array(list(map(float, report.strip().split('\n')[-1].split()[3:])))
+
+    return loss_and_metrics_train, loss_and_metrics_test, auc_score, pr_scores
 
 if __name__ == "__main__":
     n_folds = 10
@@ -103,13 +118,15 @@ if __name__ == "__main__":
     skf = StratifiedKFold(n_splits=n_folds, shuffle=True)
     train_stats = np.zeros((n_folds, 2))
     test_stats = np.zeros((n_folds, 2))
+    roc_stats = np.zeros((n_folds,))
+    pr_stats = np.zeros((n_folds, 4))
 
     i = 0
     for train_index, test_index in skf.split(data, labels[:,0]): # Silly munging of shape
             print("Running Fold", i+1, "/", n_folds)
             model = None # Clearing the NN.
             model = create_model(data.shape[1], labels.shape[1])
-            train_stats[i],test_stats[i] = train_and_evaluate_model(model, data[train_index], labels[train_index], data[test_index], labels[test_index])
+            train_stats[i],test_stats[i],roc_stats[i],pr_stats[i] = train_and_evaluate_model(model, data[train_index], labels[train_index], data[test_index], labels[test_index])
             i+=1
 
     train_mean = train_stats.mean(axis=0)
@@ -118,11 +135,24 @@ if __name__ == "__main__":
     test_mean = test_stats.mean(axis=0)
     test_std = test_stats.std(axis=0)
 
+    roc_mean = roc_stats.mean()
+    roc_std = roc_stats.std()
+
+    pr_mean = pr_stats.mean(axis=0)
+    pr_std = pr_stats.std(axis=0)
+
     result_str = """
     Training Accuracy Mean = {:1.4f} ± {:1.4f}
     Training Loss Mean = {:1.4f} ± {:1.4f}
 
     Test Accuracy Mean = {:.4f} ± {:1.4f}
     Test Loss Mean = {:1.4f} ± {:1.4f}
-    """.format(train_mean[1], train_std[1], train_mean[0], train_std[0], test_mean[1], test_std[1], test_mean[0], test_std[0])
+    Test ROC Area = {:1.4f} ± {:1.4f} 
+    Test Precision Mean = {:1.4f} ± {:1.4f}  
+    Test Recall Mean = {:1.4f} ± {:1.4f} 
+
+    """.format(train_mean[1], train_std[1], train_mean[0], train_std[0], \
+               test_mean[1], test_std[1], test_mean[0], test_std[0], \
+               roc_mean, roc_std, \
+               pr_mean[0], pr_std[0], pr_mean[1], pr_std[1])
     print(result_str)
